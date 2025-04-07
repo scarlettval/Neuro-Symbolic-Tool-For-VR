8c9b309e5475855ecbe83c6b8c94465d6df37b7d
Logan Marrone <lmarrone2021@fau.edu>
2025-04-06 23:32:04 -0400

    Create VR Rig and Screenshot code
    "We are in the Endgame now."

---
+++ neuro-symbolic-vr-tool/UnityProject/My project/Assets/scripts/ScreenshotVR.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScreenshotVR : MonoBehaviour
+{
+     public Camera vrCamera;
+    private string screenshotPath;
+
+    void Start()
+    {
+        screenshotPath = Application.persistentDataPath + "/vr_screenshot.png";
+    }
+
+    public void TakeScreenshot()
+    {
+        if (vrCamera != null)
+        {
+            RenderTexture renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+            vrCamera.targetTexture = renderTexture;
+            Texture2D screenshot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+            vrCamera.Render();
+            RenderTexture.active = renderTexture;
+            screenshot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+            screenshot.Apply();
+            byte[] bytes = screenshot.EncodeToPNG();
+            System.IO.File.WriteAllBytes(screenshotPath, bytes);
+            vrCamera.targetTexture = null;
+            RenderTexture.active = null;
+            Destroy(renderTexture);
+            Debug.Log("Screenshot saved to: " + screenshotPath);
+        }
+        else
+        {
+            Debug.LogError("VR Camera is not assigned!");
+        }
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            TakeScreenshot();
+        }
+    }
+}
