6b9a5ae49e7b02d6391e838f8a112e9bbcf4c76d
Matthew Henao <mjhenao152@gmail.com>
2025-04-14 20:29:04 -0400

    Version 1.0.2

--- neuro-symbolic-vr-tool/python/symbolic_module/rules.pl
+++ neuro-symbolic-vr-tool/python/symbolic_module/rules.pl
@@ -1,69 +1,66 @@
-% ===============================================
-% Symbolic Rules for Neuro-Symbolic VR Tool
-% ===============================================
-loaded_rules :- format('✅ rules.pl successfully loaded~n').
-python_alive :- format('🐍 Python connected to correct rules.pl~n').
+% =============================
+% Dynamic Symbolic Rules
+% =============================
 
 :- dynamic object/2.
 
-% Initial scene objects (optional)
+% Example object knowledge base
 object(small_red_cube, [size(small), color(red), shape(cube)]).
-object(medium_blue_sphere, [size(medium), color(blue), shape(sphere)]).
-object(large_green_cylinder, [size(large), color(green), shape(cylinder)]).
 
-% ---------------------------
-% Symbolic Actions
-% ---------------------------
+% ----------------------------
+% Action Definitions
+% ----------------------------
 
 move_object(Name, Direction) :-
     object(Name, _),
-    format('Moving ~w to the ~w~n', [Name, Direction]).
+    format('Moving ~w to ~w~n', [Name, Direction]).
 
-create_object(Name, Properties) :-
+create_object(Name, [size(Size), color(Color), shape(Shape)]) :-
     \+ object(Name, _),
-    assertz(object(Name, Properties)),
-    format('Created ~w with properties ~w~n', [Name, Properties]).
+    assertz(object(Name, [size(Size), color(Color), shape(Shape)])),
+    format('Created ~w~n', [Name]).
 
 delete_object(Name) :-
     object(Name, _),
     retract(object(Name, _)),
     format('Deleted ~w~n', [Name]).
 
-% ---------------------------
-% Helper to build names like small_red_cube
-% ---------------------------
+% ----------------------------
+% Object Name Builder
+% ----------------------------
 
 make_object_name(Size, Color, Shape, Name) :-
     atomic_list_concat([Size, Color, Shape], '_', Name).
 
-% ---------------------------
-% Command Parsing
-% ---------------------------
+% ----------------------------
+% interpret/2 Rules
+% ----------------------------
 
-% Interpret: move the SIZE COLOR SHAPE to DIRECTION
-interpret(CommandStr, move_object(Name, DirectionAtom)) :-
-    split_string(CommandStr, " ", "", Parts),
-    Parts = ["move", "the", SizeStr, ColorStr, ShapeStr, "to", DirectionStr],
+% move the small red cube to left
+interpret(CommandStr, move_object(Name, Direction)) :-
+    split_string(CommandStr, " ", "", Words),
+    Words = ["move", "the", SizeStr, ColorStr, ShapeStr, "to", DirectionStr],
     atom_string(Size, SizeStr),
     atom_string(Color, ColorStr),
     atom_string(Shape, ShapeStr),
-    atom_string(DirectionAtom, DirectionStr),
+    atom_string(Direction, DirectionStr),
     make_object_name(Size, Color, Shape, Name),
     object(Name, _).
 
-
+% create the small red cube
 interpret(CommandStr, create_object(Name, [size(Size), color(Color), shape(Shape)])) :-
-    split_string(CommandStr, " ", "", Parts),
-    Parts = ["create", "the", SizeStr, ColorStr, ShapeStr],
+    split_string(CommandStr, " ", "", Words),
+    Words = ["create", "the", SizeStr, ColorStr, ShapeStr],
     atom_string(Size, SizeStr),
     atom_string(Color, ColorStr),
     atom_string(Shape, ShapeStr),
     make_object_name(Size, Color, Shape, Name),
     \+ object(Name, _).
 
+% delete the small red cube
 interpret(CommandStr, delete_object(Name)) :-
-    split_string(CommandStr, " ", "", Parts),
-    Parts = ["delete", "the", SizeStr, ColorStr, ShapeStr],
+    split_string(CommandStr, " ", "", Words),
+    Words = ["delete", "the", SizeStr, ColorStr, ShapeStr],
     atom_string(Size, SizeStr),
     atom_string(Color, ColorStr),
     atom_string(Shape, ShapeStr),
